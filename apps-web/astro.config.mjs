f1463e581216c7d9956be695a96b9ac0fc8145e8
masumar <188675200+umarrudyanto@users.noreply.github.com>
2024-12-21 10:31:09 +0000

    beta-1.0.0

---
+++ apps-web/astro.config.mjs
@@ -0,0 +1,43 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import compressor from "astro-compressor";
+
+// https://astro.build/config
+export default defineConfig({
+  // https://docs.astro.build/en/guides/images/#authorizing-remote-images
+  site: "https://screwfast.uk",
+  image: {
+    domains: ["images.unsplash.com"],
+  },
+  // i18n: {
+  //   defaultLocale: "en",
+  //   locales: ["en", "fr"],
+  //   fallback: {
+  //     fr: "en",
+  //   },
+  //   routing: {
+  //     prefixDefaultLocale: false,
+  //   },
+  // },
+  prefetch: true,
+  integrations: [
+    tailwind(),
+    sitemap({
+      i18n: {
+        defaultLocale: "en", // All urls that don't contain `fr` after `https://screwfast.uk/` will be treated as default locale, i.e. `en`
+        locales: {
+          en: "en", // The `defaultLocale` value must present in `locales` keys
+          fr: "fr",
+        },
+      },
+    }),
+    compressor({
+      gzip: false,
+      brotli: true,
+    }),
+  ],
+  experimental: {
+    clientPrerender: true,
+  },
+});
